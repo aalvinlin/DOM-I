8107c81daec2c580191875d24da8ab382dcfc942
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2019-12-02 12:05:37 -0800

    Tasks 1 and 2 completed

--- js/index.js
+++ js/index.js
@@ -40,3 +40,94 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// ============ Task 1 and 2 ============ 
+
+// Add links to nav
+let navLinks = document.querySelectorAll("nav a");
+let item = 1;
+
+for (a of navLinks)
+{
+    a.textContent = siteContent.nav["nav-item-" + item];
+    item += 1;
+}
+
+// Add CTA text
+let ctaH1Formatted = siteContent.cta.h1.replace(/ /g, "<br />");
+
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.innerHTML = ctaH1Formatted;
+
+// Add CTA button
+let ctaButton = document.querySelector(".cta-text button");
+ctaButton.textContent = siteContent.cta.button;
+
+// Add CTA Image
+let ctaImage = document.getElementById("cta-img");
+ctaImage.src = siteContent.cta["img-src"];
+
+// Add Top Content section: Features and About
+let topContentDivs = document.querySelectorAll(".top-content .text-content");
+
+// Add Features section
+let featuresH4 = topContentDivs[0].querySelector("h4");
+featuresH4.textContent = siteContent["main-content"]["features-h4"];
+
+let featuresDescription = topContentDivs[0].querySelector("p");
+featuresDescription.textContent = siteContent["main-content"]["features-content"];
+
+// Add About section
+let aboutH4 = topContentDivs[1].querySelector("h4");
+aboutH4.textContent = siteContent["main-content"]["about-h4"];
+
+let aboutDescription = topContentDivs[1].querySelector("p");
+aboutDescription.textContent = siteContent["main-content"]["about-content"];
+
+// add middle image
+let middleImg = document.getElementById("middle-img");
+middleImg.src = siteContent["main-content"]["middle-img-src"];
+
+// Add Bottom Content section: Services, Product, Vision
+let bottomContentDivs = document.querySelectorAll(".bottom-content .text-content");
+
+// Add Serivces section
+let servicesH4 = bottomContentDivs[0].querySelector("h4");
+servicesH4.textContent = siteContent["main-content"]["services-h4"];
+
+let servicesDescription = bottomContentDivs[0].querySelector("p");
+servicesDescription.textContent = siteContent["main-content"]["services-content"];
+
+// Add Product section
+let productH4 = bottomContentDivs[1].querySelector("h4");
+productH4.textContent = siteContent["main-content"]["product-h4"];
+
+let productDescription = bottomContentDivs[1].querySelector("p");
+productDescription.textContent = siteContent["main-content"]["product-content"];
+
+// Add Vision section
+let visionH4 = bottomContentDivs[2].querySelector("h4");
+visionH4.textContent = siteContent["main-content"]["vision-h4"];
+
+let visionDescription = bottomContentDivs[2].querySelector("p");
+visionDescription.textContent = siteContent["main-content"]["vision-content"];
+
+// Add contact section
+let contactH4 = document.querySelector(".contact h4");
+contactH4.textContent = siteContent.contact["contact-h4"];
+
+// Add address, phone number, and email
+let contactParagraphs = document.querySelectorAll(".contact p");
+
+let addressFormatted = siteContent.contact.address.replace("Street", "Street<br>")
+
+contactParagraphs[0].innerHTML = addressFormatted;
+contactParagraphs[1].textContent = siteContent.contact.phone;
+contactParagraphs[2].textContent = siteContent.contact.email;
+
+// Add footer
+let copyright = document.querySelector("footer p");
+copyright.textContent = siteContent.footer.copyright;
+
+// ============ Task 3 ============ 
+
